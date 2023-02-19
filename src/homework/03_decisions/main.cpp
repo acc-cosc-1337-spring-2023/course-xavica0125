8302360f5289826f3167b767edd659b9ee32d7cf
xavica0125 <123427635+xavica0125@users.noreply.github.com>
2023-02-19 02:29:54 +0000

    Homework 3 Decisions - Xavier Cabrera Alvarez

--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -7,21 +7,23 @@ using std::cout; using std::cin;
 
 int main() 
 {	
-	int menu_option;
-	cout<<"MAIN MENU"<<"\n";
-	cout<<"1-Letter grade using if"<<"\n";
-	cout<<"2-Letter grade using switch"<<"\n";
-	cout<<"3-Exit"<<"\n";
+	auto menu_option= 0;
+	cout<<"MAIN MENU\n";
+	cout<<"1-Letter grade using if\n";
+	cout<<"2-Letter grade using switch\n";
+	cout<<"3-Exit\n";
 	cout<<"Select Menu Option: ";
 	cin>>menu_option;
-	if (menu_option == 1 || 2)
+	if (menu_option == 1 || menu_option == 2)
 	{
+
+		
 		int grade_number;
 		cout<<"Enter number 0-100: ";
 		cin>>grade_number;
 		if(grade_number < 0 || grade_number > 100)
 		{
-			cout<<"Number is out of range."<<"\n";
+			cout<<"Number is out of range.\n";
 			
 		}
 		else
@@ -34,12 +36,16 @@ int main()
 				case 2:
 					cout<<get_letter_grade_using_switch(grade_number)<<"\n";
 				break;
-		
 			}
 		}
 	}
-
-
-
+	
 	return 0;
-}
+
+}			
+				
+	
+
+
+
+	
